9ff0e22fde95a79488d37a4b5cda7c73a395b89f
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-01-19 23:33:59 -0800

    Create hello6.c

---
+++ hello6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,num;
+scanf("%d",&n);
+while(n>0)
+{
+scanf("%d",&num");
+while(num>0)
+{
+printf("hello");
+num--;
+}
+n--;
+}
+getch();
+}
